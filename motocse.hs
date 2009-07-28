6e3c12f46af5813419c7a6366748c5889c7f7a78
Andrew Miller <ak.miller@auckland.ac.nz>
2009-07-28 14:48:32 +0900

    Got the program working to the extent it can determine the number of true and false positives and negatives.

--- motocse.hs
+++ motocse.hs
@@ -1,9 +1,12 @@
+{-# OPTIONS_GHC -XBangPatterns #-}
 -- Motif Occurrence Tool Comparison Suite
 import Text.ParserCombinators.Parsec
 import System.Directory
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.List
 import Control.Monad
+import Debug.Trace
 
 fsaDirectory = "/home/andrew/Documents/fsafiles/"
 inputPredictionFile = "/home/andrew/Documents/gmatim/hits.fasta"
@@ -51,7 +54,7 @@ scorePredictions goldStandard predictions =
     ()
 
 -- This gives us a list of pairs, the first member is the probe, and the second is the list of all genes corresponding to that probe.
-loadGenesForProbesFromFile = loadFromFileErrorCheck (genesForAllProbes [])
+loadGenesForProbesFromFile = loadFromFileErrorCheck (geneForAllProbes [])
 geneForAllProbes l =
     (
      do
@@ -59,7 +62,7 @@ geneForAllProbes l =
        probe <- many $ noneOf "\n"
        char '\n'
        genes <- genesForSingleProbe []
-       geneForAllProbes (probe, genes):l
+       geneForAllProbes ((probe, genes):l)
     ) <|>
     (
      do
@@ -68,13 +71,48 @@ geneForAllProbes l =
     )
 genesForSingleProbe l =
     (
-     geneFirst <- noneOf ">\n"
-     geneRest <- many $ noneOf "\n"
-     char '\n'
-     genesForSingleProbe ((geneFirst:geneRest):l)
+     do
+       geneFirst <- noneOf ">\n"
+       geneRest <- many $ noneOf "\n"
+       char '\n'
+       genesForSingleProbe ((geneFirst:geneRest):l)
     ) <|> (return l)
 
-testPredictions goldStandard compareTo
+flipPair (x, y) = (y, x)
+pairListToAssociationSet = foldl' addPairListByFirstToSet S.empty
+addPairListByFirstToSet s (f, l) = foldl' (\s' -> \v -> S.insert (f, v) s') s l
+
+setCrossProduct x y = appendListCrossProduct [] (S.toList x) (S.toList y)
+mapAndPrepend f v iv = foldl' (\x -> \y -> (f y):x ) iv v
+appendListCrossProduct l [] y = l
+appendListCrossProduct l (xs:x) y = (appendListCrossProduct (mapAndPrepend ((,)xs) y l) x y)
+
+updateClassificationCounts standardSet testSet ((!tp, !fp), (!tn, !fn)) v =
+    if S.member v testSet then
+        if S.member v standardSet then
+           (((tp + 1), fp), (tn, fn))
+        else
+            ((tp, (fp + 1)), (tn, fn))
+    else
+        if S.member v standardSet then
+           ((tp, fp), (tn, (fn + 1)))
+        else
+            ((tp, fp), ((tn + 1), fn))
+
+testPredictions :: [(String, [String])] -> [(String, [String])] -> ((Int, Int), (Int, Int))
+testPredictions goldStandard compareTo =
+    trace "Entering testPredictions" $ trace "Finished testPredictions" $!
+    let
+        -- In both sets of pairs, make the gene the first member, and the probe the second.
+        goldStandardPairs = trace "Making goldStandardPairs" $ trace "Done making goldStandardPairs" $! pairListToAssociationSet goldStandard
+        compareToPairs = S.map flipPair $ pairListToAssociationSet compareTo
+        -- Get a set of all genes in the gold standard data.
+        allGenes = S.map fst goldStandardPairs
+        -- Get a set of all probes in the gold standard data.
+        allProbes = S.map snd goldStandardPairs
+        allPairs = setCrossProduct allGenes allProbes
+    in
+      foldl' (updateClassificationCounts goldStandardPairs compareToPairs) ((0, 0), (0, 0)) allPairs
 
 main = do
   files <- getDirectoryContents fsaDirectory
@@ -88,5 +126,7 @@ main = do
                 return (gene, probes)
   predictBoundByGene <-
       loadGenesForProbesFromFile inputPredictionFile
-  ((truePositves, falsePositives), (trueNegatives, falseNegatives)) <- testPredictions boundByGene predictBoundByGene
-  putStrLn $ show boundByGene
+  let
+      ((truePositives, falsePositives), (trueNegatives, falseNegatives)) = testPredictions boundByGene predictBoundByGene
+    in
+      putStrLn $ (showString "TruePositives = " . shows truePositives . showString "; TrueNegatives = " . shows trueNegatives . showString "; FalsePositives = " . shows falsePositives . showString "; FalseNegatives = " . shows falseNegatives) ""
