b91c8bbde35144ffa259b61e658a831571bbb48e
Andrew <andrew@amxl.com>
2010-01-19 13:39:26 +1300

    Version of motocse actually used in paper

--- motocse.hs
+++ motocse.hs
@@ -1,6 +1,5 @@
 {-# OPTIONS_GHC -XBangPatterns #-}
 -- Motif Occurrence Tool Comparison Suite
-import Text.ParserCombinators.Parsec
 import System.Directory
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -10,73 +9,14 @@ import System.Environment
 import System.Console.GetOpt
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Traversable as T
-
-unsafeRight (Left _) = error "Expected Left operand in unsafeEitherFirst"
-unsafeRight (Right x) = x
-
-parseOutGeneName = do
-  genename <- many (noneOf "_")
-  remainder <- many (anyChar)
-  return (genename, genename ++ remainder)
-
-addOrUpdateGroup v Nothing = Just [v]
-addOrUpdateGroup v (Just x) = Just (v:x)
-groupByFirstMap :: Ord a => [(a, b)] -> M.Map a [b]
-groupByFirstMap = foldl' (\m -> \(k, v) -> M.alter (addOrUpdateGroup v) k m) M.empty
-
-groupByFirst :: Ord a => [(a, b)] -> [(a, [b])]
-groupByFirst = M.toList . groupByFirstMap
-
-loadFromFileErrorCheck m filename =
-    do
-      probes <- parseFromFile m filename
-      case probes of
-        Left err ->
-            do
-              putStrLn $ (showString "Parse error: " . shows err) ""
-              return []
-        Right x -> return x
-
--- This gives use a list of strings - probes in the file.
-loadProbesFromFile = loadFromFileErrorCheck (probeExtractor [])
-
-probeExtractor prev =
-    (
-     do
-       char '>'
-       probe <- many $ noneOf " \n"
-       many $ noneOf ">"
-       probeExtractor (probe:prev)
-    ) <|> (eof >> return prev)
+import Data.Maybe
+import Debug.Trace
+import Probes
+import Text.ParserCombinators.Parsec
 
 scorePredictions :: [(String, [String])] -> [(String, [String])] -> ()
 scorePredictions goldStandard predictions =
     ()
-
--- This gives us a list of pairs, the first member is the probe, and the second is the list of all genes corresponding to that probe.
-loadGenesForProbesFromFile = loadFromFileErrorCheck (geneForAllProbes [])
-geneForAllProbes l =
-    (
-     do
-       char '>'
-       probe <- many $ noneOf "\n"
-       char '\n'
-       genes <- genesForSingleProbe []
-       geneForAllProbes ((probe, genes):l)
-    ) <|>
-    (
-     do
-       eof
-       return l
-    )
-genesForSingleProbe l =
-    (
-     do
-       geneFirst <- noneOf ">\n"
-       geneRest <- many $ noneOf "\n"
-       char '\n'
-       genesForSingleProbe ((geneFirst:geneRest):l)
-    ) <|> (return l)
 
 flipPair (x, y) = (y, x)
 pairListToAssociationSet = foldl' addPairListByFirstToSet S.empty
@@ -156,9 +96,13 @@ mainWithOptions fsaDirectory inputPredictionFile includeTFFile =
                 probes <- liftM concat $ mapM (loadProbesFromFile . (fsaDirectory++)) filenames
                 return (gene, probes)
       predictBoundByGene <- loadGenesForProbesFromFile inputPredictionFile
-      let
-          ((truePositives, falsePositives), (trueNegatives, falseNegatives)) = testPredictions includeTFs (map fst filesByGene) boundByGene predictBoundByGene
-        in
-          do
-            putStrLn $ (showString "TruePositives = " . shows truePositives . showString "; TrueNegatives = " . shows trueNegatives . showString "; FalsePositives = " . shows falsePositives . showString "; FalseNegatives = " . shows falseNegatives) ""
-            putStrLn $ (showString "TPR = " . shows (100.0 * (fromIntegral truePositives) / (fromIntegral (truePositives + falseNegatives))) . showString "; FPR = " . shows (100.0 * (fromIntegral falsePositives) / (fromIntegral (falsePositives + trueNegatives)))) ""
+      let allTFs = (nub . map fst) filesByGeneFiltered
+      let allProbes = nub . concat $ map snd boundByGene
+      let ((truePositives, falsePositives), (trueNegatives, falseNegatives)) =
+            testPredictions allTFs allProbes boundByGene predictBoundByGene
+      putStrLn $ (showString "TF count = " . shows (length allTFs) . showString " & probe count = " . shows (length allProbes)) ""
+      putStrLn $ (showString "TruePositives = " . shows truePositives .
+                  showString "; TrueNegatives = " . shows trueNegatives .
+                  showString "; FalsePositives = " . shows falsePositives .
+                  showString "; FalseNegatives = " . shows falseNegatives) ""
+      putStrLn $ (showString "TPR = " . shows (100.0 * (fromIntegral truePositives) / (fromIntegral (truePositives + falseNegatives))) . showString "; FPR = " . shows (100.0 * (fromIntegral falsePositives) / (fromIntegral (falsePositives + trueNegatives)))) ""
